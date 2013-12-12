052b1ff160e3d6545374c9a68bd651cc6f9e102e
Massimiliano Leone <maximilianus@gmail.com>
2013-12-12 11:15:30 +0100

    more rendering features

--- src/render/net/iubris/aa/spheres/render/stddraw3d/SpheresRender.java
+++ src/render/net/iubris/aa/spheres/render/stddraw3d/SpheresRender.java
@@ -8,6 +8,7 @@ import net.iubris.aa.spheres.model.DimensionBounds;
 import net.iubris.aa.spheres.model.RandomPoint;
 import net.iubris.aa.spheres.model.Sphere;
 import edu.princeton.stddraw3d.StdDraw3D;
+import edu.princeton.stddraw3d.StdDraw3D.Shape;
 
 public class SpheresRender {
 
@@ -18,21 +19,58 @@ public class SpheresRender {
 				bb.getYDimension().getMax()-bb.getYDimension().getMin());
 		double max = Math.max(maxXY,
 				bb.getZDimension().getMax()-bb.getZDimension().getMin());*/
-		StdDraw3D.setCanvasSize(800, 600);
+		StdDraw3D.setCanvasSize(500, 300);
 		int scale = 20;
 		StdDraw3D.setScale(-scale,scale);
 		
-		double[] axesUsed = new double[scale];
-		double[] axesNotUsed = new double[scale];
-		for (int i=0;i<scale;i++) {
-			axesUsed[i]=i;
+		//int axisLength=scale*2+1;
+		//double[] axesUsed = new double[axisLength];
+		//double[] axesNotUsed = new double[axisLength];
+		//double v=(-scale);
+		/*for (double d: axesUsed) {
+			d=v;
+			v++;
+		}
+		for (double d: axesNotUsed) {
+			d=0;
+		}*/
+		/*
+		for (int i=0;i<axisLength;i++) {
+			axesUsed[i]=v;
+			v++;
 			axesNotUsed[i]=0;
 		}
-		StdDraw3D.lines(axesUsed, axesNotUsed, axesNotUsed).setColor(Color.WHITE);
-		StdDraw3D.lines(axesNotUsed, axesUsed, axesNotUsed).setColor(Color.WHITE);
-		StdDraw3D.lines(axesNotUsed, axesNotUsed, axesUsed).setColor(Color.WHITE);
+		for (int i=0;i<axisLength;i++) {
+			System.out.print(axesUsed[i]+" ");
+		}
+		for (int i=0;i<axisLength;i++) {
+			System.out.print(axesNotUsed[i]+" ");
+		}
+		*/
+/*		for (int i=0;i<scale*2;i++) {
+			if (i<(scale-1)) {
+				axesUsed[i]=i;
+				axesNotUsed[i]=0;
+			}
+			System.out.println(i);
+		}*/
+		double[] axesUsed = new double[2];
+		axesUsed[0]=-20;
+		axesUsed[1]=20;
+		double[] axesNotUsed = new double[2]; //(0, 0);
+
+		StdDraw3D.lines(axesUsed, axesNotUsed, axesNotUsed)
+			.setColor(Color.YELLOW);
+		StdDraw3D.lines(axesNotUsed, axesUsed, axesNotUsed)
+			.setColor(Color.YELLOW);
+		StdDraw3D.lines(axesNotUsed, axesNotUsed, axesUsed)
+			.setColor(Color.YELLOW);
 		
-		StdDraw3D.text3D(0, 0, 0, "0");
+//		StdDraw3D.setPenRadius(3.0);
+		Shape textCenter = StdDraw3D.text3D(0, 0, 0, "0");
+		textCenter.setColor(Color.YELLOW);
+		textCenter.scale(5);
+//		textCenter.setPenRadius(3.0);
 		for (int l=-scale;l<=scale;l=l+2) {
 			StdDraw3D.text3D(l, 0, 0, ""+l);
 			StdDraw3D.text3D(0, l, 0, ""+l);
@@ -69,13 +107,14 @@ public class SpheresRender {
 				wbHeight,				
 				wbDepth
 				).setColor(Color.RED, 130);
+		
 		for (Sphere s: sv.getSpheres()) {
-			StdDraw3D.wireSphere(s.getX(), s.getY(), s.getZ(), s.getRadius()).setColor(Color.BLUE, 50);
-			/*Shape wireSphere = StdDraw3D.wireSphere(0, 0, 0, s.getRadius());
+		//	StdDraw3D.wireSphere(s.getX(), s.getY(), s.getZ(), s.getRadius()).setColor(Color.BLUE, 50);
+			Shape wireSphere = StdDraw3D.wireSphere(0, 0, 0, s.getRadius());
 			wireSphere.setColor(Color.BLUE, 50);
 			Vector3D vector3D = new Vector3D(s.getX(), s.getY(), s.getZ());
 			StdDraw3D.pause(5);
-			wireSphere.move(vector3D);*/			
+			wireSphere.move(vector3D);
 		}
 //		int rn=0;
 		RandomPoint[] randomPoints = sv.getRandomPoints();
@@ -85,7 +124,6 @@ public class SpheresRender {
 		double[] rzs = new double[randomPointsHow];
 //		for (int r=0;i<ranRandomPoint r: sv.getRandomPoints()) {
 		for (int i=0;i<randomPointsHow;i++) {
-		
 //			System.out.println(rn+": "+r);
 //			rn++;
 //			StdDraw3D.point(r.getX(), r.getY(), r.getZ()).setColor(Color.GREEN, 255);
@@ -94,7 +132,8 @@ public class SpheresRender {
 			rys[i] = randomPoint.getY();
 			rzs[i] = randomPoint.getZ();
 		}
-		StdDraw3D.points(rxs, rys, rzs);
+		StdDraw3D.points(rxs, rys, rzs).setColor(Color.GREEN, 255);
+
 		StdDraw3D.finished();
 		
 		System.out.println("");
